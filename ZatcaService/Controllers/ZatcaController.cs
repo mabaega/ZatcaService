ec28c5331c36d82c9c81134c08c9baab3555cdbf
mabaega <mabaega@gmail.com>
2024-06-06 21:21:05 +0700

    Add project files.

---
+++ ZatcaService/Controllers/ZatcaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZatcaService.Controllers
+{
+    public class ZatcaController : Controller
+    {
+        public IActionResult RelaySetting()
+        {
+            return View();
+        }
+
+        public IActionResult SignedInvoices()
+        {
+            return View();
+        }
+    }
+}
